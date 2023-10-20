eeb46bea558dde3cdd501c69a069d90adc05b22c
forme008 <estellaxabibullaeva0308@gmail.com>
2023-10-20 14:08:51 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,116 @@
+// // sinf ishi 1
+
+// function but1Func() {
+//     document.body.style.backgroundColor = "green"
+// }
+
+// function but2Func() {
+//     document.body.style.backgroundColor = "orange"
+// }
+
+// function but3Func() {
+//     document.body.style.backgroundColor = "black"
+// }
+
+// function but4Func() {
+//     document.body.style.backgroundColor = "pink"
+// }
+
+// function but5Func() {
+//     document.body.style.backgroundColor = "purple"
+// }
+
+
+// // sinf ishi 2
+
+// let fontSFunc = 16
+// const but6Func = function () {
+//     fontSFunc = fontSFunc + 2
+//     document.body.style.fontSize = fontSFunc + "px"
+// }
+
+// let fontFunc = 16
+// const but7Func = function () {
+//     fontFunc = fontFunc - 2
+//     document.body.style.fontSize = fontFunc + "px"
+// }
+
+// // sinf ishi 3
+
+// function colorFunc(){
+//     let color = prompt("rangni kriting")
+//     document.body.style = `background-color: ${color}`
+// }
+
+
+
+// Homeworks
+
+// firs task
+const but1Func = function () {
+   document.body.style.background = "green "
+}
+// second task 
+const but2Func = function () {
+   document.body.style.background = "orange"
+}
+// third task
+const but3Func = function () {
+   document.body.style.background = "black"
+   document.body.style.color = "white"
+}
+//forth task
+function day() {
+   document.body.style.backgroundColor = "black"
+}
+function night() {
+   document.body.style.backgroundColor = "white"
+}
+//fifth task
+const but4Func = function () {
+   document.body.style.backgroundColor = "pink"
+}
+// six
+const but5Func = function () {
+   document.body.style.backgroundColor = "purple"
+}
+
+//seven
+const plural = function () {
+}
+
+//eight
+const RedNOranche = function () {
+   document.body.style.backgroundColor = "red"
+   document.body.style.color = "orange"
+
+}
+// nine
+const me = function () {
+
+   document.body.style.fontSize = "25px"
+}
+// ten
+
+const you = function () {
+   document.body.style.fontSize = "12px"
+}
+
+//eleven
+const we = function () {
+   document.body.style.fontSize = "16px"
+}
+
+// twelve
+let fontSFunc = 16
+const plurall = function () {
+   fontSFunc = fontSFunc + 2
+   document.body.style.fontSize = fontSFunc + "px"
+}
+//thirteen
+let fontFunc = 16
+const singular = function () {
+   fontFunc = fontFunc - 2
+   document.body.style.fontSize = fontFunc + "px"
+}
+
